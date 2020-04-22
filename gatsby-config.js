fff2a9f0fe9120130ee15231f1a117edeee5da5f
Gavin Nelson <gavin@Gavins-Work-MacBook-Pro.local>
2020-04-22 17:37:46 -0400

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
+        icon: 'src/images/icon.png'
       },
     },
     {
