7f9e8ffa22ba2aa1b55da1d384448a5f13b04e2f
Gavin Nelson <gavin@Gavins-Work-MacBook-Pro.local>
2020-05-05 19:13:44 -0400

    edited node config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({
+  actions //, stage, getConfig, rules, loaders, 
+}) => {
+  actions.setWebpackConfig({
+    externals: ['canvas'],
+  });
+}
