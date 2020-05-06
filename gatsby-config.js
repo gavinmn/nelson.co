437bf17731da47f115d8d34ced36ac179e9543d5
Gavin Nelson <gavin@Gavins-Work-MacBook-Pro.local>
2020-05-06 12:01:15 -0400

    removed incompatible favicon.ico

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.ico`
+        icon: `src/images/favicon.png`
         icons: [
           {
             src: `/favicons/favicon-16x16.png`,
