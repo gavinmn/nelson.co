81199deede8c06e1fbbc6d67a2ae5a37093396dc
Gavin Nelson <gavin@Gavins-Work-MacBook-Pro.local>
2020-05-05 23:40:51 -0400

    more fixing favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,32 +36,32 @@ module.exports = {
         display: `minimal-ui`,
         icons: [
           {
-            src: `/favicons/favicon-16.png`,
+            src: `/favicons/favicon-16x16.png`,
             sizes: `16x16`,
             type: `image/png`,
           },
           {
-            src: `/favicons/favicon-32.png`,
+            src: `/favicons/favicon-32x32.png`,
             sizes: `32x32`,
             type: `image/png`,
           },
           {
-            src: `/favicons/favicon-48.png`,
+            src: `/favicons/favicon-48x48.png`,
             sizes: `48x48`,
             type: `image/png`,
           },
           {
-            src: `/favicons/apple-touch-icon-180.png`,
+            src: `/favicons/apple-touch-icon-180x180.png`,
             sizes: `180x180`,
             type: `image/png`,
           },
            {
-            src: `/favicons/android-chrome-192.png`,
+            src: `/favicons/android-chrome-192x192.png`,
             sizes: `192x192`,
             type: `image/png`,
           },
           {
-            src: `/favicons/android-chrome-512.png`,
+            src: `/favicons/android-chrome-512x512.png`,
             sizes: `512x512`,
             type: `image/png`,
           },
