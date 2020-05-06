7ca9897d80ad61343d7e1d12086583851a9445f6
Gavin Nelson <gavin@Gavins-Work-MacBook-Pro.local>
2020-05-05 21:46:13 -0400

    updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Gavin Nelson Design`,
+        short_name: `GNd`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
