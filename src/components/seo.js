17152414da72d1bc789f1c0407774d1cc9797429
Gavin Nelson <gnelsondesign@gmail.com>
2020-05-10 20:02:16 -0400

    fixed small error in image

--- src/components/seo.js
+++ src/components/seo.js
@@ -29,9 +29,7 @@ function SEO({ description, lang, meta, title, image: metaImage }) {
 
   const metaDescription = description || site.siteMetadata.description
   const image =
-    metaImage && metaImage.src
-      ? `${site.siteMetadata.siteUrl}/images/og.png`
-      : null
+    metaImage && metaImage.src ? `${site.siteMetadata.siteUrl}${image}` : null
 
   return (
     <Helmet
