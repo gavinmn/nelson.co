235fde634f067e8bb329c78abd9769ff2f68d387
Gavin Nelson <gnelsondesign@gmail.com>
2020-05-24 17:43:19 -0400

    edited env variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `${process.env.GATRACKINGID}`,
+        trackingId: `${process.env.GA_ID}`,
       },
     },
     "gatsby-plugin-offline",
