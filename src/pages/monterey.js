f08c8adfd7f34a623276e50e18fd7483cad230cb
Gavin Nelson <gnelsondesign@gmail.com>
2020-06-28 22:52:43 -0400

    solidified coming soon layout

--- src/pages/monterey.js
+++ src/pages/monterey.js
@@ -7,42 +7,85 @@ import SEO from "../components/seo"
 import { device } from "../components/device"
 import Wrapper from "../components/wrapper"
 import "./fonts.css"
+import bgimage from "../images/monterey/montereybackground.jpg"
 
-const HeroContainer = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  margin-bottom: 4rem;
-
-  @media ${device.desktop} {
-    margin-bottom: 2rem;
-  }
+const BGImage = styled.div`
+  background-image: url(${bgimage});
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: auto;
 `
-const Grid = styled.div`
+
+const FullView = styled.div`
   display: grid;
-  grid-template-columns: auto;
-  grid-template-rows: auto;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
 `
 
-const BackgroundImage = styled(Img)`
-  width: 100vw;
-  grid-column: 1;
+const CenterContainer = styled.div`
+  width: 100%;
+  align-self: center;
+  justify-self: center;
   grid-row: 1;
+  grid-column: 1;
+`
+
+const IconContainer = styled.div`
+  width: 100%;
+  max-width: 816px;
+  display: flex;
+  justify-content: space-around;
+`
+
+const IconImage = styled(Img)`
+  width: 4rem;
+  height: 4rem;
+  pointer-events: none;
 `
 
 const MontereyText = styled.p`
-  font-size: 5rem;
+  font-size: 4rem;
   font-weight: bold;
   color: #ffffff;
-  grid-column: 1;
-  grid-row: 1;
-  align-self: center;
+`
+
+const DescriptionText = styled.p`
+  font-size: 1rem;
+  color: #ffffff;
   justify-self: center;
-  z-index: 2;
+  align-self: end;
+  grid-row: 1;
+  grid-column: 1;
+  margin-bottom: 1rem;
+`
+// const SecondaryContent = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   grid-column: 1;
+//   grid-row: 2;
+//   display: grid;
+//   grid-template-columns: auto;
+//   grid-template-rows: auto auto;
+// `
+
+const DetailHeader = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin: 0 auto;
+  text-align: center;
+  max-width: 630px;
+  margin-top: 4rem;
+  margin-bottom: 1rem;
+`
+
+const DetailText = styled.p`
+  margin: 0 auto 4rem auto;
+  text-align: center;
+  max-width: 630px;
 `
 const Twitter = styled.a`
-  ${"" /* color: var(--wash-primary); */}
   font-size: 1rem;
   &:hover {
     color: #1da1f2;
@@ -53,55 +96,55 @@ const Twitter = styled.a`
   }
 `
 
-const DescriptionText = styled.p`
-  font-size: 1rem;
-  color: #ffffff;
-  padding-bottom: 2rem;
-  grid-column: 1;
-  grid-row: 1;
-  align-self: end;
-  justify-self: center;
-  z-index: 2;
-`
-const DetailHeader = styled.p`
-  margin: 0 auto;
-  text-align: center;
-  max-width: 630px;
-  font-size: 2rem;
-  font-weight: bold;
-  margin-bottom: 1rem;
-`
-
-const DetailText = styled.p`
-  margin: 0 auto;
-  text-align: center;
-  max-width: 630px;
-  margin-bottom: 4rem;
-`
-
 const MontereyPage = props => {
   return (
-    <Layout location={props.location}>
-      <SEO title="Monterey Big Sur Theme - " />
-      <HeroContainer>
-        <Grid>
-          <MontereyText>Monterey</MontereyText>
-          <DescriptionText>
-            A macOS Big Sur icon theme by Gavin Nelson
-          </DescriptionText>
-          <BackgroundImage
-            fluid={props.data.montereyBackground.childImageSharp.fluid}
-            alt=""
-          />
-        </Grid>
-      </HeroContainer>
-      <DetailHeader>Coming Soon</DetailHeader>
-      <DetailText>
-        Follow Gavin on{" "}
-        <Twitter href="https://twitter.com/Gavmn">Twitter </Twitter>to be
-        notified of release.
-      </DetailText>
-    </Layout>
+    <BGImage>
+      <Layout location={props.location}>
+        <SEO title="Monterey Big Sur Theme - " />
+        <Wrapper>
+          <FullView>
+            <CenterContainer>
+              <IconContainer>
+                <IconImage
+                  fluid={props.data.notes.childImageSharp.fluid}
+                  alt=""
+                />
+                <IconImage
+                  fluid={props.data.notes.childImageSharp.fluid}
+                  alt=""
+                />
+                <IconImage
+                  fluid={props.data.notes.childImageSharp.fluid}
+                  alt=""
+                />
+              </IconContainer>
+              <MontereyText>Monterey</MontereyText>
+              <IconContainer>
+                <IconImage
+                  fluid={props.data.notes.childImageSharp.fluid}
+                  alt=""
+                />
+                <IconImage
+                  fluid={props.data.notes.childImageSharp.fluid}
+                  alt=""
+                />
+                <IconImage
+                  fluid={props.data.notes.childImageSharp.fluid}
+                  alt=""
+                />
+              </IconContainer>
+            </CenterContainer>
+            <DescriptionText>A macOS Big Sur icon theme</DescriptionText>
+          </FullView>
+          <DetailHeader>Coming Soon</DetailHeader>
+          <DetailText>
+            Follow Gavin on{" "}
+            <Twitter href="https://twitter.com/Gavmn">Twitter </Twitter>to be
+            notified of release.
+          </DetailText>
+        </Wrapper>
+      </Layout>
+    </BGImage>
   )
 }
 
@@ -114,6 +157,13 @@ export const PageQuery = graphql`
     ) {
       childImageSharp {
         fluid(maxWidth: 6010) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    notes: file(relativePath: { eq: "monterey/Notes.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 256) {
           ...GatsbyImageSharpFluid_withWebp
         }
       }
