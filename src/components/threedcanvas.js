f0c0699c9e66e4051a3bd587c1d3e28c4fe11c96
Gavin Nelson <gnelsondesign@gmail.com>
2020-09-23 13:23:26 -0700

    added suspense component

--- src/components/threedcanvas.js
+++ src/components/threedcanvas.js
@@ -4,6 +4,22 @@ import { OrbitControls, PerspectiveCamera } from "drei"
 import "../pages/styles.css"
 import Model from "../../herothreed"
 
+// function Loading() {
+//   return (
+//     <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]} scale={[2, 2, 0]}>
+//       <sphereGeometry attach="geometry" args={[1, 32, 32]} />
+//       <meshStandardMaterial
+//         attach="material"
+//         color="black"
+//         transparent
+//         opacity={0.4}
+//         roughness={1}
+//         metalness={0}
+//       />
+//     </mesh>
+//   )
+// }
+
 function ThreeDCanvas() {
   if (typeof window !== `undefined`) {
     var pixelRatio = window.devicePixelRatio
