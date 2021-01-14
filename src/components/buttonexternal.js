507ea86210e16ceeeec46c1f4a1b99377ab98496
Gavin Nelson <gnelsondesign@gmail.com>
2021-01-14 03:53:21 +0000

    revamped external button

--- src/components/buttonexternal.js
+++ src/components/buttonexternal.js
@@ -3,47 +3,34 @@ import Link from "next/link"
 import styled from "styled-components"
 import { device } from "./device"
 
-const ButtonStyle = styled.a`
-  padding: 1rem;
-  max-width: 100%;
-  background: ${props => props.backgroundColor || "var(--color-primary)"};
-  border-radius: 8px;
-  transition: all 0.1s ease-in-out;
+const Button = styled.a`
   display: flex;
-  align-items: center;
+  flex-grow: 0;
+  flex-direction: column;
   justify-content: center;
-  font-size: 16px;
-  font-weight: bold;
-  color: var(--bg-primary-hex);
+  align-items: center;
+  padding: 8px 20px;
+  background-color: var(--blue-secondary);
+  border-radius: 4px;
 
   &:hover {
     cursor: pointer;
-    background: var(--bg-primary-hex);
-    color: ${props => props.backgroundColor || "var(--color-primary)"};
-    box-shadow: 0 0 0 1px
-      ${props => props.backgroundColor || "var(--color-primary)"} inset;
-  }
-
-  @media only screen and (min-width: 445px) {
-    max-width: 10rem;
-  }
-
-  @media ${device.desktop} {
-    padding: 0.5rem;
-    font-size: 18px;
-    max-width: 6rem;
+    background-color: var(--blue-primary);
+    & > p {
+      color: var(--bg-primary-hex);
+    }
   }
 `
 
+const Arrow = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  color: var(--blue-primary);
+`
+
 const ButtonExternal = props => (
-  <ButtonStyle
-    href={props.href}
-    backgroundColor={props.color}
-    hoverBackground={props.hoverBackground}
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    {props.text}
-  </ButtonStyle>
+  <Button href={props.link}>
+    <Arrow>→</Arrow>
+  </Button>
 )
 export default ButtonExternal
