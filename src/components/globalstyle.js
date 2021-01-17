6c6b67c15a66d6806d56c8bcf043e34d96ff2e4e
Gavin Nelson <gnelsondesign@gmail.com>
2021-01-17 20:11:29 +0000

    dark mode theme change

--- src/components/globalstyle.js
+++ src/components/globalstyle.js
@@ -2,9 +2,8 @@ import { createGlobalStyle } from "styled-components"
 export const GlobalStyle = createGlobalStyle`
 	
 	* {
- 		margin:0;
-  		padding:0;
-		  -webkit-font-smoothing: antialiased;
+		 margin:0;
+		  padding:0;
 	}
 	
 	html {
@@ -17,15 +16,15 @@ export const GlobalStyle = createGlobalStyle`
 		
 		--bg-secondary: #F5F7FA;
 		
-		--text-primary: #0F1A24;
+		--text-primary: #1A2028;
 		--text-secondary: #517095;
 		
 		--blue-primary: #007AFF;
 		--blue-secondary: rgba(10,122,255,.1);
 		
-		// used for button hover states and logo
-		--color-primary: #0F1A24;
-		--logo-primary: #0F1A24;
+		// used for button hover states and logo (can remove)
+		--color-primary: #1A2028;
+		--logo-primary: #1A2028;
 
 		--color-chalk: #006EF0;
 		--color-chalk-secondary: #F1F8FF;
@@ -34,39 +33,16 @@ export const GlobalStyle = createGlobalStyle`
 		--color-experts-secondary: #F1FBF3;
 		
 		--github-text: #24292E;
-		
-		--font-xs: 14px;
-		--font-s: 16px;
-		--font-m: 18px;
-		--font-l: 26px;
-		
-		--lh-xs: 20px;
-		--lh-s: 24px;
-		--lh-m: 24px;
-		--lh-l: 30px;
-		
-		@media only screen and (min-width: 722px) { 
-			--font-xs: 16px;
-			--font-s: 18px;
-			--font-m: 20px;
-			--font-l: 28px;
-			
-			--lh-xs: 24px;
-			--lh-s: 24px;
-			--lh-m: 28px;
-			--lh-l: 36px;
-		}
-		
 
 		@media(prefers-color-scheme: dark) {
 			
-			--bg-primary-hex: #040506;
-			--bg-primary-rgba: rgba(4,5,6,0.8);
+			--bg-primary-hex: #212831;
+			--bg-primary-rgba: rgba(33,40,49,0.8);
 			
-			--bg-secondary: #090F15;
+			--bg-secondary: #1A2028;
 
 			--text-primary: #F5F7FA;
-			--text-secondary: #667D99; 
+			--text-secondary: #7A91AE; 
 			
 			--blue-primary: #0A84FF;
 			--blue-secondary: rgba(10,132,255,.1);
@@ -89,11 +65,11 @@ export const GlobalStyle = createGlobalStyle`
 	}
 
 	body {
-	  	background-color: var(--bg-primary-hex);
-	  	font-family: 'Sohne', -apple-system, BlinkMacSystemFont, sans-serif;
-	  	color: var(--text-primary);
-	  	overflow-y: scroll;
-	  	overflow-x: auto;
+		  background-color: var(--bg-primary-hex);
+		  font-family: 'Sohne', -apple-system, BlinkMacSystemFont, sans-serif;
+		  color: var(--text-primary);
+		  overflow-y: scroll;
+		  overflow-x: auto;
 		-webkit-overflow-scrolling: touch;
 	}
 
@@ -104,37 +80,37 @@ export const GlobalStyle = createGlobalStyle`
 
 	html, body {
 		width: 100%;
-	 	height: 100%
+		 height: 100%
 	}
 
 	html {
 		font-size: 16px;
-	  	-webkit-overflow-scrolling: touch;
-	  	-webkit-font-smoothing: antialiased;
-	  	text-rendering: optimizeLegibility;
-	  	line-height: 1.4;
+		  -webkit-overflow-scrolling: touch;
+		  -webkit-font-smoothing: antialiased;
+		  text-rendering: optimizeLegibility;
+		  line-height: 1.5;
 	}
 
 	footer {
-	  	margin-bottom:2rem;
-	  	font-size: 14px;
-  		color: var(--text-secondary);
+		  margin-bottom:2rem;
+		  font-size: 14px;
+		  color: var(--text-secondary);
 	}
 
 	img {
-	  	max-width: 100%;
-	  	height:auto;
+		  max-width: 100%;
+		  height:auto;
 	}
 	
 
 	::selection {
 		color: white;
-	  	background: var(--color-primary);
+		  background: var(--color-primary);
 	}
 
 	@media only screen and (min-width: 722px) {
-  		html {
-    		font-size: 32px;
-  		}
-  	}
+		  html {
+			font-size: 32px;
+		  }
+	  }
 `
