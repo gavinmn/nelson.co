bb7e088706b4b2327fcb05109b2e94b61e8b831a
Gavin Nelson <gnelsondesign@gmail.com>
2021-01-20 16:11:13 +0000

    edited font preloads

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -35,14 +35,24 @@ export default class MyDocument extends Document {
             rel="preload"
             href="/fonts/soehne-web-buch-kursiv.woff2"
             as="font"
+            type="font/woff2"
+            crossorigin
           />
 
-          <link rel="preload" href="/fonts/soehne-web-buch.woff2" as="font" />
+          <link
+            rel="preload"
+            href="/fonts/soehne-web-buch.woff2"
+            as="font"
+            type="font/woff2"
+            crossorigin
+          />
 
           <link
             rel="preload"
             href="/fonts/soehne-web-dreiviertelfett.woff2"
             as="font"
+            type="font/woff2"
+            crossorigin
           />
 
           <link rel="dns-prefetch" href="https://gumroad.com/l/dvctd" />
