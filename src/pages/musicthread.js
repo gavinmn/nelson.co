a968fc7f7a017503d5327784da8b7ce807bd42b6
Gavin Nelson <gnelsondesign@gmail.com>
2021-01-25 22:09:53 +0000

    added wrappers

--- src/pages/musicthread.js
+++ src/pages/musicthread.js
@@ -1,21 +1,36 @@
 import React from "react"
 import styled from "styled-components"
 import { device } from "../components/device"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Wrapper from "../components/wrapper"
 import Image from "next/image"
 
+const Container = styled.div`
+  margin: 4rem 0;
+`
+
 const MusicThread = ({ data }) => {
+  console.log({ data })
+
   return (
-    <div>
-      {data.links.map((data, key) => {
-        return (
-          <div key={key}>
-            <p>{data.title}</p>
-            <p>{data.artist}</p>
-            <Image src={data.thumbnail_url} layout="fill" />
-          </div>
-        )
-      })}
-    </div>
+    <Layout>
+      <SEO title="MusicThread" />
+      <Wrapper size="small">
+        <Container>
+          <a src={data.thread.page_url}>{data.thread.title}</a>
+          {data.links.map((data, key) => {
+            return (
+              <div key={key}>
+                <p>{data.title}</p>
+                <p>{data.artist}</p>
+                <Image src={data.thumbnail_url} width={200} height={200} />
+              </div>
+            )
+          })}
+        </Container>
+      </Wrapper>
+    </Layout>
   )
 }
 
