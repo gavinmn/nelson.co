a76fd38f9e4595d4f0d893550903176b42e19eef
Gavin Nelson <gnelsondesign@gmail.com>
2021-01-25 21:59:10 +0000

    got JSON data in

--- src/pages/musicthread.js
+++ src/pages/musicthread.js
@@ -1,10 +1,22 @@
 import React from "react"
 import styled from "styled-components"
 import { device } from "../components/device"
+import Image from "next/image"
 
 const MusicThread = ({ data }) => {
-  console.log(data.links[1].artist)
-  return <div></div>
+  return (
+    <div>
+      {data.links.map((data, key) => {
+        return (
+          <div key={key}>
+            <p>{data.title}</p>
+            <p>{data.artist}</p>
+            <Image src={data.thumbnail_url} layout="fill" />
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export async function getServerSideProps() {
