e7e69e0621d4b826760a4ca42b5308ee1f57c1ac
Gavin Nelson <gnelsondesign@gmail.com>
2021-01-26 00:25:41 +0000

    edited title area

--- src/pages/musicthread.js
+++ src/pages/musicthread.js
@@ -10,6 +10,13 @@ import Caption from "../components/caption"
 const Container = styled.div`
   margin: 4rem 0;
 `
+const MUsicThreadLink = styled.a`
+  font-size: var(--font-xs);
+  color: var(--text-secondary);
+  &:hover {
+    color: var(--text-primary);
+  }
+`
 
 const MusicThread = ({ data }) => {
   console.log({ data })
@@ -19,10 +26,16 @@ const MusicThread = ({ data }) => {
       <SEO title="MusicThread" />
       <Wrapper size="small">
         <Container>
-          <a href={data.thread.page_url}>{data.thread.title}</a>
+          <h1>Heavy Rotation</h1>
           <Caption>
-            Powered by <a href="https://musicthread.app/">MusicThread</a>
+            A thread of recent favorite tracks, powered by{" "}
+            <MUsicThreadLink href="https://musicthread.app/">
+              MusicThread
+            </MUsicThreadLink>
           </Caption>
+
+          {/* <a href={data.thread.page_url}>{data.thread.title}</a> */}
+
           {data.links.reverse().map((data, key) => {
             return (
               <div key={key}>
