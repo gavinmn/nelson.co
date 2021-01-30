8bf2af1a321fcfdd26a3db7ced841fd074758ee4
Gavin Nelson <gnelsondesign@gmail.com>
2021-01-30 03:10:25 +0000

    got the styles working

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,13 +1,16 @@
 import matter from "gray-matter"
-
+import renderToString from "next-mdx-remote/render-to-string"
+import hydrate from "next-mdx-remote/hydrate"
 import { getAllPostSlugs, getPostdata } from "../../lib/posts"
+
+// const components = { MyBackground, MyButton }
 
 import PostWrapper from "@/components/postwrapper"
 
 export default function Posts({ source, frontMatter }) {
-  return <PostWrapper>{source}</PostWrapper>
+  const content = hydrate(source)
+  return <PostWrapper>{content}</PostWrapper>
 }
-
 export async function getStaticPaths() {
   const paths = getAllPostSlugs()
   return {
@@ -18,9 +21,12 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   const postContent = await getPostdata(params.slug)
   const { data, content } = matter(postContent)
+  const mdxSource = await renderToString(content, {
+    scope: data,
+  })
   return {
     props: {
-      source: content,
+      source: mdxSource,
       frontMatter: data,
     },
   }
