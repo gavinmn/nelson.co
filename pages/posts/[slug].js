9c003b2c3ca07d85a5d0b2bb8cd9ba55b4a710d0
Gavin Nelson <gnelsondesign@gmail.com>
2021-01-30 03:07:09 +0000

    got back to where I was but with lib file

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,43 +1,27 @@
-import fs from "fs"
-import path from "path"
 import matter from "gray-matter"
 
+import { getAllPostSlugs, getPostdata } from "../../lib/posts"
+
 import PostWrapper from "@/components/postwrapper"
-export default function Post({ content, frontmatter }) {
-  console.log(frontmatter)
-  return <PostWrapper>{content}</PostWrapper>
+
+export default function Posts({ source, frontMatter }) {
+  return <PostWrapper>{source}</PostWrapper>
 }
 
 export async function getStaticPaths() {
-  const files = fs.readdirSync("src/data/posts")
-
-  const paths = files.map(filename => ({
-    params: {
-      slug: filename.replace(".mdx", ""),
-    },
-  }))
-
+  const paths = getAllPostSlugs()
   return {
     paths,
     fallback: false,
   }
 }
-
-export async function getStaticProps({ params: { slug } }) {
-  const markdownWithMetadata = fs
-    .readFileSync(path.join("src/data/posts", slug + ".mdx"))
-    .toString()
-
-  const { data, content } = matter(markdownWithMetadata)
-
-  const frontmatter = {
-    data,
-  }
-
+export async function getStaticProps({ params }) {
+  const postContent = await getPostdata(params.slug)
+  const { data, content } = matter(postContent)
   return {
     props: {
-      content: content,
-      frontmatter,
+      source: content,
+      frontMatter: data,
     },
   }
 }
