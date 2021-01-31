2b8a0c27cf62ebb62fa76cad9311af1e18ae9772
Gavin Nelson <gnelsondesign@gmail.com>
2021-01-31 01:30:27 +0000

    fixed wrapper bug

--- components/customimage.js
+++ components/customimage.js
@@ -3,21 +3,6 @@ import styled from "styled-components"
 import Image from "next/image"
 import { device } from "./device"
 
-const CustomImageContainer = styled.div`
-  width: 100%;
-  margin: 2rem auto;
-  max-width: ${props => (props.full ? "816px" : "444px")};
-
-  @media ${device.desktop} {
-    margin: ${props => (props.full ? "1rem auto" : "1rem auto")};
-  }
-
-  @media only screen and (min-width: 848px) {
-    width: ${props => (props.full ? "200%" : "")};
-    margin: ${props => (props.full ? "1rem -3rem" : "1rem auto")};
-  }
-`
-
 const CustomImage = props => {
   const [full, setFull] = useState(false)
 
--- components/postwrapper.js
+++ components/postwrapper.js
@@ -9,41 +9,6 @@ import Wrapper from "@/components/wrapper"
 const PostWrapper = props => {
   return (
     <Layout>
-      <SEO title={props.title} og={props.og} description={props.description} />
-      <Wrapper className="wrapper-large">
-        <Fade>
-          <div className="container">
-            <Wrapper className="wrapper-small">
-              {props.children}
-              <div className="feedback">
-                <h3 className="feedback-title">Feedback</h3>
-                <p>
-                  Do you have comments or questions about something I wrote?
-                  Send them my way on{" "}
-                  <a
-                    className="twitter"
-                    href="https://twitter.com/Gavmn"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Twitter
-                  </a>{" "}
-                  or over{" "}
-                  <a
-                    className="email"
-                    href="mailto:gavin@nelson.co"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    email
-                  </a>
-                  .
-                </p>
-              </div>
-            </Wrapper>
-          </div>
-        </Fade>
-      </Wrapper>
       <style jsx>{`
         .wrapper-large {
           max-width: 816px;
@@ -92,6 +57,41 @@ const PostWrapper = props => {
           }
         }
       `}</style>
+      <SEO title={props.title} og={props.og} description={props.description} />
+      <div className="wrapper-large">
+        <Fade>
+          <div className="container">
+            <div className="wrapper-small">
+              {props.children}
+              <div className="feedback">
+                <h3 className="feedback-title">Feedback</h3>
+                <p>
+                  Do you have comments or questions about something I wrote?
+                  Send them my way on{" "}
+                  <a
+                    className="twitter"
+                    href="https://twitter.com/Gavmn"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Twitter
+                  </a>{" "}
+                  or over{" "}
+                  <a
+                    className="email"
+                    href="mailto:gavin@nelson.co"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    email
+                  </a>
+                  .
+                </p>
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </div>
     </Layout>
   )
 }
