70d3df14bacf8e6c0119f778ce344d9b31729609
Gavin Nelson <gavin@nelson.co>
2021-05-15 22:36:11 -0400

    added spacing units

--- components/globalstyle.js
+++ components/globalstyle.js
@@ -14,6 +14,15 @@ function GlobalStyle(props) {
         }
 
         :root {
+          --sp-1: 0.25rem;
+          --sp-2: 0.5rem;
+          --sp-3: 0.75rem;
+          --sp-4: 1rem;
+          --sp-5: 1.25rem;
+          --sp-6: 1.5rem;
+          --sp-7: 1.75rem;
+          --sp-8: 2rem;
+
           --body: 15px;
           --small: 14px;
           --h1: 24px;
@@ -99,7 +108,7 @@ function GlobalStyle(props) {
           -webkit-overflow-scrolling: touch;
           -webkit-font-smoothing: antialiased;
           text-rendering: optimizeLegibility;
-          line-height: 1.6;
+          line-height: 1.5;
         }
 
         footer {
