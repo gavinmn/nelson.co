a05cfb4dc3cd5fb53a7743134068779bb1d97c32
Gavin Nelson <gavin@nelson.co>
2021-05-26 16:48:43 -0400

    edited colophon

--- pages/colophon.js
+++ pages/colophon.js
@@ -3,7 +3,6 @@ import Layout from "@/components/layout"
 import SEO from "@/components/seo"
 import { device } from "@/components/device"
 
-
 const Colophon = props => (
   <Layout>
     <style jsx>{`
@@ -33,70 +32,61 @@ const Colophon = props => (
       description="The making of nelson.co"
     />
 
-      <div className="text-container">
-        <h1>Colophon</h1>
+    <div className="text-container">
+      <h1>Colophon</h1>
 
-        <p>
-          This website is designed and developed by Gavin Nelson in San
-          Francisco, California. <br></br>
-          <br></br>It is built using{" "}
-          <a href="https://nova.app" target="_blank" rel="noopener noreferrer">
-            Nova
-          </a>{" "}
-          by Panic, statically generated through{" "}
-          <a
-            href="https://nextjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Next.js
-          </a>
-          , and deployed using{" "}
-          <a
-            href="https://vercel.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vercel
-          </a>
-          . It is designed in both{" "}
-          <a
-            href="https://www.sketch.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Sketch
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://www.figma.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Figma
-          </a>{" "}
-          and the type is set in Söhne by{" "}
-          <a
-            href="https://klim.co.nz"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Klim Type Foundry
-          </a>
-          .<br></br>
-          <br></br>
-          The source code that powers it can be found on{" "}
-          <a
-            href="https://github.com/gavinmn/nelson.co"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-          . The content on this website is copyrighted.
-        </p>
-      </div>
-
+      <p>
+        This website is designed and developed by Gavin Nelson in San Francisco,
+        California. <br></br>
+        <br></br>It is built using{" "}
+        <a
+          href="https://code.visualstudio.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          VS Code
+        </a>
+        , statically generated through{" "}
+        <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer">
+          Next.js
+        </a>
+        , and deployed using{" "}
+        <a href="https://vercel.com" target="_blank" rel="noopener noreferrer">
+          Vercel
+        </a>
+        . It is designed in both{" "}
+        <a
+          href="https://www.sketch.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sketch
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://www.figma.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Figma
+        </a>{" "}
+        and the type is set in Söhne by{" "}
+        <a href="https://klim.co.nz" target="_blank" rel="noopener noreferrer">
+          Klim Type Foundry
+        </a>
+        .<br></br>
+        <br></br>
+        The source code that powers it can be found on{" "}
+        <a
+          href="https://github.com/gavinmn/nelson.co"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        . The content on this website is copyrighted.
+      </p>
+    </div>
   </Layout>
 )
 
