ef85876d2964ab8824fff89cf5e3084d4b980048
Gavin Nelson <gavin@nelson.co>
2021-05-27 22:17:42 -0400

    displaying highlight data

--- pages/highlights.js
+++ pages/highlights.js
@@ -1,5 +1,6 @@
 import Layout from "@/components/layout"
 import SEO from "@/components/seo"
+import { device } from "@/components/device"
 
 const Highlights = ({ highlights, books }) => {
   console.log(highlights)
@@ -7,6 +8,38 @@ const Highlights = ({ highlights, books }) => {
   return (
     <Layout>
       <SEO />
+      <div className="wrapper-large">
+        <div className="container">
+          <div className="wrapper-small">
+            {highlights.results.map((item, highlight) => {
+              return <p>{highlights.results[highlight].text}</p>
+            })}
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .wrapper-large {
+          max-width: 656px;
+          padding: 0 5% 0 5%;
+          margin: 0 auto;
+        }
+
+        .wrapper-small {
+          max-width: 576px;
+          padding: 0;
+          margin: 0 auto;
+        }
+
+        .container {
+          margin: 8rem 0 0rem 0;
+        }
+
+        @media ${device.desktop} {
+          .container {
+            margin: 4rem 0 0rem 0;
+          }
+        }
+      `}</style>
     </Layout>
   )
 }
