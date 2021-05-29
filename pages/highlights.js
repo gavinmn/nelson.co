e66b5db618ba3289038b99cd8ff9e9ef8df6ad2a
Gavin Nelson <gavin@nelson.co>
2021-05-29 18:26:54 -0400

    conditional title based on highlight position

--- pages/highlights.js
+++ pages/highlights.js
@@ -1,10 +1,12 @@
+import Link from "next/link"
+
 import Layout from "@/components/layout"
 import SEO from "@/components/seo"
 import { device } from "@/components/device"
 
 const Highlights = ({ highlights, books }) => {
-  // console.log(books)
-  // console.log(highlights)
+  console.log(books)
+  console.log(highlights)
 
   const monthNames = [
     "January",
@@ -32,14 +34,19 @@ const Highlights = ({ highlights, books }) => {
               const title = books.results[book].title
               const author = books.results[book].author
               const bookID = books.results[book].id
+              const sourceLink = books.results[book].source_url
 
               var numHighlights = 0
+              var firstHighlight = true
 
-              if (category != "tweets") {
-                return (
-                  <>
-                    <p className="title">{title}</p>
+              return (
+                <>
+                  <div className="source-container">
+                    <a className="title" href={sourceLink}>
+                      {title}
+                    </a>
                     <p className="author">{author}</p>
+
                     {highlights.results.map((item, highlight) => {
                       const highlightSource =
                         highlights.results[highlight].book_id
@@ -48,8 +55,12 @@ const Highlights = ({ highlights, books }) => {
                       const highlightTime =
                         highlights.results[highlight].highlighted_at
 
-                      if (highlightSource == bookID && numHighlights < 3) {
+                      if (highlightSource == bookID && numHighlights < 8) {
                         numHighlights++
+
+                        if (numHighlights > 1) {
+                          firstHighlight = false
+                        }
 
                         const date = new Date(highlightTime)
 
@@ -59,15 +70,33 @@ const Highlights = ({ highlights, books }) => {
 
                         return (
                           <>
+                            <Link href={`#`}>
+                              <></>
+                            </Link>
+                            <a
+                              className={`${
+                                firstHighlight ? "title hidden" : "title"
+                              }`}
+                              href={sourceLink}
+                            >
+                              {title}
+                            </a>
+                            <p
+                              className={`${
+                                firstHighlight ? "author hidden" : "author"
+                              }`}
+                            >
+                              {author}
+                            </p>
                             <p className="highlight">{highlightText}</p>
                             <p className="date">{dateStamp}</p>
                           </>
                         )
                       }
                     })}
-                  </>
-                )
-              }
+                  </div>
+                </>
+              )
             })}
           </div>
         </div>
@@ -89,6 +118,16 @@ const Highlights = ({ highlights, books }) => {
           margin: 8rem 0 0rem 0;
         }
 
+        .source-container {
+        }
+
+        .line {
+          background-color: var(--primary-200);
+          width: 2px;
+          align-self: stretch;
+          margin-top: -1.5rem;
+        }
+
         .title {
           display: inline-block;
           font-size: var(--h3);
@@ -98,6 +137,11 @@ const Highlights = ({ highlights, books }) => {
         .author {
           color: var(--accent-100);
           margin-bottom: 0.25rem;
+        }
+
+        .title.hidden,
+        .author.hidden {
+          display: none;
         }
 
         .highlight {
@@ -133,7 +177,7 @@ export async function getStaticProps() {
   const highlights = await highlightsResponse.json()
 
   const booksResponse = await fetch(
-    `https://readwise.io/api/v2/books/`,
+    `https://readwise.io/api/v2/books/?category=articles`,
     headers
   )
 
