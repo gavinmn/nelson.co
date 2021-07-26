56e875fcfafcc902bcebb5888fc7b1d50fc0b184
Gavin Nelson <gavin@nelson.co>
2021-07-25 22:10:41 -0700

    setup preview

---
+++ pages/notes.js
@@ -0,0 +1,47 @@
+import Layout from "@/components/layout"
+import SEO from "@/components/seo"
+
+const Notes = ({ notes }) => {
+  return (
+    <Layout>
+      <SEO
+        title={"Gavin's notes"}
+        og={"notesog.png"}
+        description={"A digital garden, currently being planted"}
+      />
+      <div className="container">
+        <div className="wrapper">
+          <div className="preview">
+            <p className="emoji">🌱</p>
+            <p className="text">Planting in progress</p>
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .container {
+          margin: 4rem 0 0rem 0;
+        }
+        .wrapper {
+          max-width: 656px;
+          padding: 0 5% 0 5%;
+          margin: 0 auto;
+        }
+        .preview {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          height: 80vh;
+        }
+        .emoji {
+          font-size: 2rem;
+        }
+        .text {
+          color: var(--text-accent);
+        }
+      `}</style>
+    </Layout>
+  )
+}
+
+export default Notes
