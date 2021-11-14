6ac290fbc4032403a188b79dc925d7190375a4f7
Gavin Nelson <gavin@nelson.co>
2021-11-13 21:10:00 -0800

    add svg

---
+++ components/arrowdiagonal.js
@@ -0,0 +1,37 @@
+const Arrow = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 28 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      x="18.6066"
+      y="7.64648"
+      width="2"
+      height="16"
+      rx="1"
+      transform="rotate(45 18.6066 7.64648)"
+      fill="var(--purple-300)"
+    />
+    <rect
+      x="19.6672"
+      y="8"
+      width="2"
+      height="10"
+      rx="1"
+      transform="rotate(90 19.6672 8)"
+      fill="var(--purple-300)"
+    />
+    <rect
+      width="2"
+      height="10"
+      rx="1"
+      transform="matrix(-1 0 0 1 19.6672 8)"
+      fill="var(--purple-300)"
+    />
+  </svg>
+)
+
+export default Arrow
