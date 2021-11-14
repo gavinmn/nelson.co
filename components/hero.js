ee8a73faf4ea9bb6e06f5785ce3855e9d7eb47fe
Gavin Nelson <gavin@nelson.co>
2021-11-13 18:08:17 -0800

    add base darkmode

--- components/hero.js
+++ components/hero.js
@@ -6,10 +6,10 @@ const Hero = props => {
       <div className="grid mt-16 md:mt-64 auto-rows-auto gap-y-4">
         <Logo />
         <div>
-          <h1 className="font-sans font-medium text-primary">Gavin Nelson</h1>
-          <p className="font-sans text-secondary">Product & Icon Designer</p>
+          <h1 className="font-medium ">Gavin Nelson</h1>
+          <h2>Product & Icon Designer</h2>
         </div>
-        <p className="font-sans text-primary">
+        <p>
           I'm currently shaping the mobile apps at GitHub and crafting beautiful
           app icons for companies and individuals. I focus on fit and finish to
           solve problems and create seamless experiences.
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,15 @@ module.exports = {
       colors: {
         primary: "hsl(224, 10%, 10%)",
         secondary: "hsl(224, 10%, 46%)",
+        "primary-dark": "hsl(0, 0%, 100%)",
+        background: "hsl(0, 0%, 100%)",
+        "background-dark": "hsl(224, 25%, 12%)",
       },
       textColor: {
         primary: "hsl(224, 10%, 10%)",
         secondary: "hsl(224, 10%, 46%)",
+        "primary-dark": "hsl(0, 0%, 100%)",
+        "secondary-dark": "hsl(224, 25%, 46%)",
       },
     },
     fontFamily: {
