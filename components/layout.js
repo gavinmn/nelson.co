fdb944158da94bd9f67be62c6aa358675184d529
Gavin Nelson <gavin@nelson.co>
2021-11-20 22:26:08 -0800

    404 tailwind migration

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import Header from "./header"
 const Layout = ({ children }) => {
   return (
     <main>
-      <div className="mx-auto my-16 max-w-container-small md:max-w-container-large md:my-32">
+      <div className="mx-auto mt-16 max-w-container-small md:max-w-container-large md:mt-32">
         <Header />
         {children}
         <div className="flex flex-row justify-between mb-8 ">
