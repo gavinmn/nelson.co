5ad640b8aee07c8007ddb4018d4c0105b0e7c600
Gavin Nelson <gavin@nelson.co>
2021-11-20 18:58:52 -0800

    update digital tool style

--- components/svg.js
+++ components/svg.js
@@ -16,6 +16,24 @@ export const ArrowRight = () => (
   </svg>
 )
 
+export const ArrowExternal = () => (
+  <svg
+    className="inline ml-1 transform -rotate-45"
+    width="11"
+    height="8"
+    viewBox="0 0 11 8"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1 3.99994H9.5M9.5 3.99994L6.5 0.999939M9.5 3.99994L6.5 7.00006"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="stroke-current text-tertiary"
+    />
+  </svg>
+)
+
 export const ArrowDown = () => (
   <svg
     className="inline ml-2"
--- components/tool.js
+++ components/tool.js
@@ -1,95 +1,18 @@
-import Image from "next/image"
+import { ArrowExternal } from "./svg"
 
 const Tool = props => (
-  <a
-    className="container"
-    href={props.link}
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <div className="icon">
-      <Image
-        src={`/images/tools/${props.image}`}
-        width="80px"
-        height="80px"
-        alt=""
-      ></Image>
-    </div>
-    <div className="text-container">
-      <div className="title-container">
-        <p className="name">{props.name}</p>
-        <p className="arrow">→</p>
-      </div>
-      <p className="description">{props.description}</p>
-    </div>
-    <style jsx>{`
-      .container {
-        display: flex;
-        flex-direction: row;
-        align-items: flex-start;
-        padding: 1rem 0;
-        border-radius: 4px;
-      }
-
-      .container:hover {
-        background-color: transparent;
-      }
-
-      @media only screen and (min-width: 768px) {
-        .container {
-          padding: 0.5rem;
-          margin: 1rem -0.5rem;
-          align-items: center;
-        }
-      }
-
-      .icon {
-        margin-right: 1rem;
-        overflow: hidden;
-        flex-shrink: 0;
-      }
-
-      .title-container {
-        display: flex;
-        flex-direction: row;
-      }
-
-      .arrow {
-        font-size: var(--body);
-        font-weight: bold;
-        color: var(--accent-100);
-        margin-left: 0.25rem;
-        opacity: 0;
-        transition-duration: 0.3s;
-      }
-
-      .container:hover > div > div > .arrow {
-        opacity: 1;
-        margin-left: 0.5rem;
-      }
-
-      @media only screen and (min-width: 768px) {
-        .icon {
-          margin-right: 0.75rem;
-        }
-
-        .arrow {
-          margin-left: 0.125rem;
-        }
-        .container:hover > div > div > .arrow {
-          margin-left: 0.25rem;
-        }
-      }
-
-      .name {
-        color: var(--text-primary);
-      }
-
-      .description {
-        color: var(--text-accent);
-      }
-    `}</style>
-  </a>
+  <div className="flex flex-col items-start py-4">
+    <a
+      className="flex flex-row items-center"
+      href={props.link}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {props.name}
+      <ArrowExternal />
+    </a>
+    <p className="text-tertiary">{props.description}</p>
+  </div>
 )
 
 export default Tool
