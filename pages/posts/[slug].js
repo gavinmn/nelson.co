1b82f902c4113e9e72a3563164796735682beca9
Gavin Nelson <gavin@nelson.co>
2021-11-20 18:58:39 -0800

    refactor postwrapper into slug

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -6,18 +6,14 @@ import fs from "fs"
 import path from "path"
 import { postFilePaths, POSTS_PATH } from "../../lib/mdxUtils"
 
-import PostWrapper from "@/components/postwrapper"
-
+import SEO from "@/components/seo"
+import Layout from "@/components/layout"
 import Image from "next/image"
-
 import CustomImage from "@/components/customimage"
 import Caption from "@/components/caption"
 import CustomLink from "@/components/customlink"
 import HorizontalRule from "@/components/horizontalrule"
 import Break from "@/components/break"
-
-import ZContainer from "@/components/posts/zcontainer"
-import Cube from "@/components/posts/cube"
 
 import Tool from "@/components/tool"
 
@@ -33,14 +29,18 @@ const components = {
 
 export default function Posts({ source, frontMatter }) {
   return (
-    <PostWrapper
-      title={frontMatter.title}
-      og={frontMatter.og}
-      description={frontMatter.subtitle}
-      time={frontMatter.time}
-    >
-      <MDXRemote {...source} components={components} />
-    </PostWrapper>
+    <Layout>
+      <SEO
+        title={frontMatter.title}
+        og={frontMatter.og}
+        description={frontMatter.description}
+      />
+      <div className="my-16 md:my-32">
+        <div>
+          <MDXRemote {...source} components={components} />
+        </div>
+      </div>
+    </Layout>
   )
 }
 
