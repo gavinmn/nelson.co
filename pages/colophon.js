f3d92a4093237d1571e9cdd88fc6815ba1a2c74b
Gavin Nelson <gavin@nelson.co>
2021-11-22 12:30:21 -0800

    code cleanup

--- pages/colophon.js
+++ pages/colophon.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "@/components/layout"
 import SEO from "@/components/seo"
 
-const Colophon = props => (
+const Colophon = () => (
   <Layout>
     <SEO
       title="Colophon"
