a59fcb521fe4af6c9777a3599f9c4ed9ebfa6f4b
Gavin Nelson <gavin@nelson.co>
2021-11-23 20:47:18 -0800

    remove unused packages

--- next.config.js
+++ next.config.js
@@ -3,13 +3,30 @@ const withMDX = require("@next/mdx")({
   extension: /\.(md|mdx)$/,
 })
 
-
-
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "mdx", "md"],
   images: {
     domains: ["img.musicthread.app"],
   },
+  webpack: (config, { dev, isServer }) => {
+    config.resolve.alias["@"] = path.resolve("./")
+
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      })
+    }
+
+    return config
+  },
+})
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+module.exports = withBundleAnalyzer({
   webpack: (config, { dev, isServer }) => {
     config.resolve.alias["@"] = path.resolve("./")
 
