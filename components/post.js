830abc16a1b23fdc87f76da7d107615ba6df2f34
Gavin Nelson <gavin@nelson.co>
2023-03-15 16:52:43 -0700

    Add features section

--- components/post.js
+++ components/post.js
@@ -13,3 +13,15 @@ const Post = props => (
 )
 
 export default Post
+
+
+export const Feature = props => (
+  <div className="grid grid-cols-118-auto">
+    <p className=" text-tertiary dark:text-darkTertiary">{props.date}</p>
+    <Link
+      href={props.href}
+      className="text-secondary dark:text-darkSecondary">
+      {props.title}
+    </Link>
+  </div>
+)
